f5df310745de620ac44c3ad89e0958e58949daf2
JKamue <mail@jkamue.de>
2020-12-23 10:15:50 +0100

    Created 480p panel

--- Zombat/Form1.Designer.cs
+++ Zombat/Form1.Designer.cs
@@ -29,13 +29,37 @@ namespace Zombat
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnlGame = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pnlGame
+            // 
+            this.pnlGame.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlGame.Location = new System.Drawing.Point(0, 0);
+            this.pnlGame.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pnlGame.Name = "pnlGame";
+            this.pnlGame.Size = new System.Drawing.Size(640, 480);
+            this.pnlGame.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(640, 480);
+            this.Controls.Add(this.pnlGame);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Zombat";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlGame;
     }
 }
 
