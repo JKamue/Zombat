65dcbb2c0c003607c06308dc332996ebd71e84ee
JKamue <mail@jkamue.de>
2020-12-25 14:56:34 +0100

    Added rays

--- Zombat/Game/Game.cs
+++ Zombat/Game/Game.cs
@@ -1,4 +1,6 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Windows.Forms;
 using Zombat.Graphics;
 
@@ -12,6 +14,11 @@ namespace Zombat.Game
         private readonly Panel _miniMap;
         private readonly BufferedScreenController _bufferedScreen;
 
+        private readonly double _fovAngle = 60 * (Math.PI / 180);
+        private readonly double _stripWidth = 1;
+        private readonly double _rayNum;
+        private readonly List<Ray> _rays = new List<Ray>();
+        
         public Game(Map map, DirectBitmap bitmap, Panel miniMap)
         {
             _map = map;
@@ -19,13 +26,29 @@ namespace Zombat.Game
             _bitmap = bitmap;
             _miniMap = miniMap;
             _bufferedScreen = new BufferedScreenController(miniMap, Color.White);
+            
             miniMap.Size = map.GetSize();
+            _rayNum = _bitmap.Bitmap.Width / _stripWidth;
         }
 
+        public void CastAllRays()
+        {
+            _rays.Clear();
+            var rayAngle = _player.Rotation - _fovAngle / 2;
+            
+            for (var i = 0; i < _rayNum; i++)
+            {
+                _rays.Add(new Ray(_player.X, _player.Y, rayAngle));
+                rayAngle += _fovAngle / _rayNum;
+            }
+        }
+        
         public void Redraw()
         {
             var minimap = _bufferedScreen.StartDrawing();
+            CastAllRays();
             _map.Redraw(minimap);
+            _rays.ForEach(r => r.Redraw(minimap));
             _player.Redraw(minimap);
             _bufferedScreen.FinishDrawing();
         }
---
+++ Zombat/Game/Ray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zombat.Game
+{
+    class Ray
+    {
+        private float x;
+        private float y;
+        private double angle;
+
+        public Ray(float x, float y, double angle)
+        {
+            this.x = x;
+            this.y = y;
+            this.angle = angle;
+        }
+
+        public void Redraw(System.Drawing.Graphics g)
+        {
+            g.DrawLine(new Pen(Color.FromArgb(10, 45, 196, 39)), x, y, x + (float)Math.Cos(angle) * 50, y + (float)Math.Sin(angle) * 50);
+        }
+    }
+}
